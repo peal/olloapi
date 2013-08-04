657593328a7906d4bb97ff8a382ed38410f8ab1c
Pelle Almquist <pelle@wrapp.com>
2013-08-03 19:55:53 -0700

    Inital commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='olloapi',
+    version='0.0.1',
+    packages=['olloapi',],
+    data_files=[],
+    zip_safe=False,
+)
